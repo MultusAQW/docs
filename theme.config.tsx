011de3b7b7dbabb2ebe2f20a8ee030f1ab15ce30
gneiru <noelrohi59@gmail.com>
2023-06-07 18:27:23 +0800

    setup config + needed components

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import { TwitterIcon } from './components/Icons';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Multus',
+        description: 'Multus | %s',
+      };
+    }
+    return {
+      titleTemplate: 'Multus',
+      description: 'Multus is a an AQW Guild based in Yorumi',
+    };
+  },
+  logo: (
+    <>
+      <img src="https://avatars.githubusercontent.com/u/135822576?s=200&v=4" alt="Multus Logo" width="36" height="36" />
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Multus
+      </span>
+    </>),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/multusaqw/api',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/multus',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/multusaqw/api',
+  gitTimestamp: <></>,
+  main: ({ children }) => <div style={{ maxWidth: 1024, margin: '0 auto' }}>{children}</div>,
+  navbar: {
+    extraContent:
+  <>
+    <TwitterIcon />
+  </>,
+  },
+  themeSwitch: {
+  },
+  darkMode: true,
+  nextThemes: {
+    defaultTheme: 'system',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  editLink: {
+    text: '',
+  },
+  head: (
+    <>
+      <link rel="icon" type="image/svg+xml" href="https://avatars.githubusercontent.com/u/135822576?s=200&v=4" />
+    </>
+  ),
   footer: {
-    text: 'Nextra Docs Template',
+    component: () => <div />,
   },
-}
 
-export default config
+};
+
+export default config;
